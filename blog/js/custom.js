73b0360b95a92650197772fe5a724aacd79c7e2d
Jae-Mar <jaemar1697@gmail.com>
2017-06-02 13:24:20 +0800

    Blog update 1

---
+++ blog/js/custom.js
@@ -0,0 +1,57 @@
+(function($) {
+    "use strict"; // Start of use strict
+    // Navigation Scripts to Show Header on Scroll-Up
+
+    var MQL = 991;
+
+    //primary navigation slide-in effect
+    if ($(window).width() > MQL) {
+        var headerHeight = $('.navbar-custom').height();
+        $(window).on('scroll', {
+                previousTop: 0
+            },
+            function() {
+                var currentTop = $(window).scrollTop();
+                //check if user is scrolling up
+                if (currentTop < this.previousTop) {
+                    //if scrolling up...
+                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+                        $('.navbar-custom').addClass('is-visible');
+                    } else {
+                        $('.navbar-custom').removeClass('is-visible is-fixed');
+                    }
+                } else if (currentTop > this.previousTop) {
+                    //if scrolling down...
+                    $('.navbar-custom').removeClass('is-visible');
+                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+                }
+                this.previousTop = currentTop;
+            });
+    }
+    //dropdown-menu on hover
+    if ($(window).width() > 767) {
+        $('ul.nav li.dropdown').hover(function() {
+            $(this).find('.dropdown-menu').stop(true, true).fadeIn();
+        }, function() {
+            $(this).find('.dropdown-menu').stop(true, true).fadeOut();
+        });
+    }
+    //dropdown-menu on hover ends
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').on('click', function() {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+    $('#back-to-top').tooltip('show');
+})(jQuery);
